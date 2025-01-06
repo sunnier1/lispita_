a2350da6d10330161d11086899d7600e0e4d686a
sunnier1 <153050560+sunnier1@users.noreply.github.com>
2025-01-06 11:09:35 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<table class="pk-table">
+    <thead>
+        <tr class="rad">
+                <th rowspan="2">NO.</th>
+                <th rowspan="2">JENIS BARANG</th>
+                <th colspan="3">SPESIFIKASI</th>
+                <th rowspan="2">NO. BARCODE</th>
+                <th rowspan="2">Action</th>
+            </tr>
+            <tr>
+                <th>JENIS/MERK/TYPE</th>
+                <th>JUMLAH</th>
+                <th>KONDISI</th>
+            </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>2</td>
+            <td>wqwe</td>
+            <td>wqwe</td>
+            <td>wqwe</td>
+            <td>wqwe</td> 
+            <td>wqwe</td> 
+            <td>wqwe</td> 
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>wqwe</td>
+            <td>wqwe</td>
+            <td>wqwe</td>
+            <td>wqwe</td> 
+            <td>wqwe</td> 
+            <td>wqwe</td> 
+        </tr>
+    </tbody>
+</table>
+</body>
+</html>
